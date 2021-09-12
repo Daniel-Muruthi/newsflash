b0f88de5af51bae801a2e29391e2b86f6a34c2e3
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-09-13 01:45:09 +0300

    This will add the configuration main class config and subclasses prod and dev

--- app/config.py
+++ app/config.py
@@ -1,1 +1,29 @@
-NEWS_API_KEY = '08db512f09b84c36a7d6f33d72d82fad'
+import os
+
+class Config:
+
+    """
+    General configuration parent class
+    """
+
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+    '''
+    pass
+
+class DevConfig(Config):
+    """
+    Development configuration child class
+    """
+    DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
+
