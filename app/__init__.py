6a1946b64e53b9842c0fbac09ffea507eb178339
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-09-14 09:18:41 +0300

    This will add navbar

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from .config import DevConfig
-
+from flask_bootstrap import Bootstrap
 
 
 # Initializing application
-app = Flask(__name__,instance_relative_config = True)
+app = Flask(__name__, instance_relative_config = True)
 
 # Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 
 from app import views
