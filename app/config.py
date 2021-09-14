5ea4cc7f3514d5389f4a6f8b4bfe4fcf6a7d21e6
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-09-14 11:38:54 +0300

    deploying to heroku

--- app/config.py
+++ app/config.py
@@ -1,8 +1,11 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
     NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines/sources?apiKey=08db512f09b84c36a7d6f33d72d82fad'
+    NEWS_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 
